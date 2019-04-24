e0a46bf06b24dbd0f23a7608a75e0d12366d1c04
Łukasz Siatka <lukasz@lukaszsiatka.pl>
2019-04-24 19:42:52 -0400

    updated knexfile - added connection details to DBs in development, test and production environments

--- knexfile.js
+++ knexfile.js
@@ -1,44 +1,51 @@
-// Update with your config settings.
+require('dotenv').config();
+
+const localPgConnection = {
+  host: 'localhost',
+  database: 'my_db',
+  user: 'username',
+  password: 'password'
+};
+
+const prodDbConnection = process.env.DATABASE_URL || localPgConnection;
 
 module.exports = {
-
   development: {
     client: 'sqlite3',
+    useNullAsDefault: true,
     connection: {
-      filename: './dev.sqlite3'
-    }
-  },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
+      filename: './src/database/dev.sqlite3'
     },
     migrations: {
-      tableName: 'knex_migrations'
+      directory: './src/database/migrations'
+    },
+    seeds: {
+      directory: './src/database/seeds'
     }
   },
 
+  test: {
+    client: 'sqlite3',
+    useNullAsDefault: true,
+    connection: {
+      filename: './src/database/test.sqlite3'
+    },
+    migrations: {
+      directory: './src/database/migrations'
+    },
+    seeds: {
+      directory: './src/database/seeds'
+    }
+  },
+  
   production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
+    client: 'pg',
+    connection: prodDbConnection,
     migrations: {
-      tableName: 'knex_migrations'
+      directory: './src/database/migrations'
+    },
+    seeds: {
+      directory: './src/database/seeds'
     }
   }
-
 };
