62df846cb00a93feef80a45f13abebe266b9deaa
Łukasz Siatka <lukasz@lukaszsiatka.pl>
2019-04-24 19:20:25 -0400

    updated index.js - added server import and listener

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log('Test');
+require('dotenv').config();
+const server = require('./src/server');
+
+const port = process.env.PORT || 5000;
+
+server.listen(port, () => {
+    console.log(`Server listening at port ${port}`);
+});
