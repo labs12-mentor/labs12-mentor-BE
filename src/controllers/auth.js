5932fbf9aef7283a2393834a4c521be0c5d1e1db
Łukasz Siatka <lukasz@lukaszsiatka.pl>
2019-04-25 14:29:00 -0400

    updated auth controller - added imports

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -3,6 +3,10 @@ module.exports = {
     registerUser
 }
 
+const Users = require('../database/helpers/users');
+const bcrypt = require('bcryptjs');
+const generateToken = require('../middleware/generateToken');
+
 async function loginUser(req, res){
     res.status(200).json({ message: 'login user API OK' });
 }
