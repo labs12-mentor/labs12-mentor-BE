6d2dadd4d2f643f635b9d6c0ae53c4498c3da57f
Łukasz Siatka <lukasz@lukaszsiatka.pl>
2019-04-25 10:52:20 -0400

    updated index controller

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,11 +1,23 @@
 const apiController = require('./api');
-const usersController = require('./users');
-const rolesController = require('./roles');
 const authController = require('./auth');
+const experiencesController = require('./experiences');
+const matchesController = require('./matches');
+const menteeProfilesController = require('./menteeProfiles');
+const mentorProfilesController = require('./mentorProfiles');
+const notificationsController = require('./notifications');
+const organizationsController = require('./organizations');
+const rolesController = require('./roles');
+const usersController = require('./users');
 
 module.exports = {
     apiController,
     authController,
-    usersController,
-    rolesController
+    experiencesController,
+    matchesController,
+    menteeProfilesController,
+    mentorProfilesController,
+    notificationsController,
+    organizationsController,
+    rolesController,
+    usersController
 }
