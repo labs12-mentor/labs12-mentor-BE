e0e82552f980f6a2a6f39f4ff821c51f22613288
Łukasz Siatka <lukasz@lukaszsiatka.pl>
2019-04-24 21:16:51 -0400

    added notifications migration

---
+++ src/database/migrations/20190424205632_notifications.js
@@ -0,0 +1,20 @@
+const { dropTable } = require('../helpers/db');
+
+exports.up = function(knex, Promise) {
+    return knex.schema.createTable('notifications', table => {
+        table
+            .increments();
+        
+        fkey(table, 'user_id', 'users');
+
+        table
+            .string('content')
+            .notNullable();
+
+        table
+            .boolean('deleted')
+            .default(false);
+    });
+};
+
+exports.down = dropTable('notifications');
