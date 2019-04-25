e68b93293b89842c1b88d2118aa7ea2a7c5fe1f5
Łukasz Siatka <lukasz@lukaszsiatka.pl>
2019-04-24 20:52:01 -0400

    updated and refactored index router

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,16 @@
 const router = require('express').Router();
 const controllers = require('../controllers');
 
-router
-    .route('/')
-        .get(controllers.apiController.rootRoute);
+const users = require('./users');
+const auth = require('./auth');
+const roles = require('./roles');
+
+router.route('/')
+    .get(controllers.apiController.rootRoute);
+
+
+router.use('/users', users);
+router.use('/auth', auth);
+router.use('/roles', roles);
 
 module.exports = router;
