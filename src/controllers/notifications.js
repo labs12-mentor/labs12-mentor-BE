9e0a887528a84e1865cb7a0d9b6c05e74b13c5bc
Łukasz Siatka <lukasz@lukaszsiatka.pl>
2019-04-25 10:53:17 -0400

    added notifications  controller

---
+++ src/controllers/notifications.js
@@ -0,0 +1,33 @@
+module.exports = {
+    getNotifications,
+    addNotification,
+    getNotification,
+    updateNotification,
+    deleteNotification,
+    removeNotification
+}
+const Notifications = require('../database/helpers/notifications');
+
+async function getNotifications(req, res){
+    res.status(200).json({ message: 'get notifications API OK' });
+}
+
+async function addNotification(req, res){
+    res.status(200).json({ message: 'add notification API OK' });
+}
+
+async function getNotification(req, res){
+    res.status(200).json({ message: 'get notification API OK' });
+}
+
+async function updateNotification(req, res){
+    res.status(200).json({ message: 'update notification API OK' });
+}
+
+async function deleteNotification(req, res){
+    res.status(200).json({ message: 'delete notification API OK' });
+}
+
+async function removeNotification(req, res){
+    res.status(200).json({ message: 'remove notification API OK' });
+}
