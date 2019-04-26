7dd4aec1dba078a7e0acc76db3665289c41057f4
Łukasz Siatka <lukasz@lukaszsiatka.pl>
2019-04-26 14:53:50 -0400

    update mentor profile migration

--- src/database/migrations/20190426145035_mentorprofiles.js
+++ src/database/migrations/20190426145035_mentorprofiles.js
@@ -1,8 +1,16 @@
+const { dropTable, fkey } = require('../helpers/db');
 
 exports.up = function(knex, Promise) {
-  
+    return knex.schema.createTable('mentorprofiles', table => {
+        table
+            .increments();
+        
+        fkey(table, 'user_id', 'users');
+        
+        table
+            .boolean('deleted')
+            .default(false);
+    });
 };
 
-exports.down = function(knex, Promise) {
-  
-};
+exports.down = dropTable('mentorprofiles');
