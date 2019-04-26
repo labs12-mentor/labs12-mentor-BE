384059a772669d67c7b5d5dda288afb10c1dfc48
Łukasz Siatka <lukasz@lukaszsiatka.pl>
2019-04-26 14:43:42 -0400

    added users migration

---
+++ src/database/migrations/20190426144241_users.js
@@ -0,0 +1,49 @@
+const { dropTable, fkey } = require('../helpers/db');
+
+exports.up = function(knex, Promise) {
+    return knex.schema.createTable('users', table => {
+        table
+            .increments();
+        
+        table
+            .string('username')
+            .notNullable()
+            .unique();
+        
+        table
+            .string('password')
+            .notNullable();
+        
+        table
+            .string('first_name')
+            .notNullable();
+        
+        table
+            .string('last_name')
+            .notNullable();
+        
+        table
+            .string('email')
+            .notNullable();
+
+        table
+            .string('country');
+        
+        table
+            .string('state');
+        
+        table
+            .string('city');
+        
+        table
+            .string('zipcode');
+
+        fkey(table, 'role_id', 'roles');
+
+        table
+            .boolean('deleted')
+            .default(false);
+    });
+};
+
+exports.down = dropTable('users');
