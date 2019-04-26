9d03dea63c21e93a483876067c79b6e0c5da4e40
Łukasz Siatka <lukasz@lukaszsiatka.pl>
2019-04-26 10:32:10 -0400

    added default users controllers methods just to display seeds content

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -8,11 +8,13 @@ module.exports = {
 const Users = require('../database/helpers/users');
 
 async function getAllUsers(req, res){
-    res.status(200).json({ message: 'get all users API OK' });
+    const users = await Users.getAllUsers();
+    res.status(200).json(users);
 }
 
 async function getUser(req, res){
-    res.status(200).json({ message: 'get user API OK' });
+    const user = await Users.getUserById(req.params.id);
+    res.status(200).json(user);
 }
 
 async function updateUser(req, res){
