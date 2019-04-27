8cf3d739168048a6e171929a68b8231bd234285a
Łukasz Siatka <lukasz@lukaszsiatka.pl>
2019-04-26 20:50:43 -0400

    added mentees seed

---
+++ src/database/seeds/007-mentees.js
@@ -0,0 +1,26 @@
+const faker = require('faker');
+
+function makeMentee(i){
+  return {
+    id: i,
+    user_id: i,
+    desired_zip: faker.address.zipCode(),
+    lambda_week: Math.round(Math.random()*30, 0),
+    interests: faker.random.words(),
+    application_answers: faker.random.words(),
+    wanted_mentor_id: Math.round(Math.random()*4, 0)
+  }
+}
+
+exports.seed = function(knex, Promise) {
+  return knex('menteeprofiles').del()
+    .then(function () {
+      const menteesList = [];
+      for(let i=0; i<10; i++){
+        menteesList.push(makeMentee(i));
+        console.log(i);
+      }
+      console.log("----- Mentees added! -----");
+      return knex('menteeprofiles').insert(menteesList);
+    });
+};
