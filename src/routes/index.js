26eadde95084e6a12b00b0dd55512a278bca1f35
Łukasz Siatka <lukasz@lukaszsiatka.pl>
2019-04-26 20:56:51 -0400

    updated index router

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const { apiController } = require('../controllers');
 
+const administrators = require('./administrators');
 const auth = require('./auth');
 const experiences = require('./experiences');
 const matches = require('./matches');
@@ -9,13 +10,14 @@ const menteeProfiles = require('./menteeProfiles');
 const mentorProfiles = require('./mentorProfiles');
 const notifications = require('./notifications');
 const organizations = require('./organizations');
+const owners = require('./owners');
 const roles = require('./roles');
 const users = require('./users');
 
 router.route('/')
     .get(apiController.rootRoute);
 
-
+router.use('/administrators', administrators);
 router.use('/auth', auth);
 router.use('/experiences', experiences);
 router.use('/matches', matches);
@@ -24,6 +26,7 @@ router.use('/mentees', menteeProfiles);
 router.use('/mentors', mentorProfiles);
 router.use('/notifications', notifications);
 router.use('/organizations', organizations);
+router.use('/owners', owners);
 router.use('/roles', roles);
 router.use('/users', users);
 
