a34e4f754021bc95869f65dc6a90d7c802602211
Jor_Poon <jormpoon@gmail.com>
2019-04-29 17:41:13 -0400

    added some entries to GET routes by id, added more tests to Post route /meetings

--- src/tests/meetings.test.js
+++ src/tests/meetings.test.js
@@ -107,6 +107,46 @@ describe('MEETINGS ROUTER', () => {
             expect(res.body).toEqual({message:'Meeting has been created'})
         })
 
+        it('should return a message on success (without notes)', async () => {
+            const res = await request(server)
+            .post(MEETING_API_URL)
+            .send({
+                match_id: 1,
+                meeting_date: '2016-02-05T03:30:17.883Z',
+                location: 'New York',
+                notes: '',
+                rating: Math.floor((Math.random()*5))
+            });
+            expect(res.body).toEqual({message:'Meeting has been created'})
+        })
+
+        it('should return a message on success (without rating)', async () => {
+            const res = await request(server)
+            .post(MEETING_API_URL)
+            .send({
+                match_id: 1,
+                meeting_date: '2016-02-05T03:30:17.883Z',
+                location: 'New York',
+                notes: 'some meeting',
+                rating: null
+            });
+            expect(res.body).toEqual({message:'Meeting has been created'})
+        })
+
+        it('should return a message on success (without notes and ratings', async () => {
+            const res = await request(server)
+            .post(MEETING_API_URL)
+            .send({
+                match_id: 1,
+                meeting_date: '2016-02-05T03:30:17.883Z',
+                location: 'New York',
+                notes: '',
+                rating: null,
+            });
+            expect(res.body).toEqual({message:'Meeting has been created'})
+        })
+
+
         it('should return status 400 on fail (no match id)', async () => {
             const res = await request(server)
             .post(MEETING_API_URL)
@@ -195,17 +235,26 @@ describe('MEETINGS ROUTER', () => {
             const res = await request(server).get(`${MEETING_API_URL}/1`);
             expect(res.status).toEqual(200);
         })
+
+        it('should return specified meeting', async () => {
+            await createMeeting();
+            const res = await request(server).get(`${MEETING_API_URL}/1`);
+            expect(res.body).toEqual({id: 1, ...sampleMeeting});
+        })
+
     })
 
     describe('PUT ROUTE /MEETINGS/:id', () => {
         it('should return status 200 on success', async () => {
+            await createMeeting();
+            
             const res = await request(server)
             .put(`${MEETING_API_URL}/1`)
             .send({
                 match_id: 1,
                 meeting_date: '2016-02-05T03:30:17.883Z',
                 location: 'New York --- MANHATTAN',
-                notes: 'some meeting',
+                notes: 'some cool meeting',
                 rating: Math.floor((Math.random()*5))
             });
 
@@ -213,13 +262,15 @@ describe('MEETINGS ROUTER', () => {
         })
 
         it('should return message on success', async () => {
+            await createMeeting();
+
             const res = await request(server)
             .put(`${MEETING_API_URL}/1`)
             .send({
                 match_id: 1,
                 meeting_date: '2016-02-05T03:30:17.883Z',
                 location: 'New York --- MANHATTAN',
-                notes: 'some meeting',
+                notes: 'some cool meeting',
                 rating: Math.floor((Math.random()*5))
             });
 
