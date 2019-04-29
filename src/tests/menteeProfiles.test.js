60973e5f5afd2806eec47b2e4ee81792b398fadb
Jor_Poon <jormpoon@gmail.com>
2019-04-29 19:56:14 -0400

    made test POST(it should return status 201 without interests

--- src/tests/menteeProfiles.test.js
+++ src/tests/menteeProfiles.test.js
@@ -38,24 +38,38 @@ describe("MENTEES ROUTER", () => {
     it("should return an empty array", async () => {
       const res = await request(server).get(MENTEE_API_URL);
       expect(res.body).toHaveLength(0);
-    })
+    });
 
     it("should return an array with length of 1", async () => {
-        await createMentee();
-        const res = await request(server).get(MENTEE_API_URL);
-        expect(res.body).toHaveLength(1);
+      await createMentee();
+      const res = await request(server).get(MENTEE_API_URL);
+      expect(res.body).toHaveLength(1);
     });
   });
 
   describe("POST ROUTE/MENTEES", () => {
-      it("should return status 201 on success", async () => {
-          const res = await request(server)
-          .post(MENTEE_API_URL)
-          .send(sampleProfile);
+    it("should return status 201 on success", async () => {
+      const res = await request(server)
+        .post(MENTEE_API_URL)
+        .send(sampleProfile);
 
-          expect(res.status).toEqual(201);
-      })
-  })
+      expect(res.status).toEqual(201);
+    });
+
+    it("should return status 201 without interests", async () => {
+      const res = await request(server)
+        .post(MENTEE_API_URL)
+        .send({
+          user_id: 1,
+          desired_zip: 11009,
+          lambda_week: 5,
+          interests: "",
+          application_answers: "yes",
+          wanted_mentor_id: 6
+        });
+      expect(res.status).toEqual(201);
+    });
+  });
 
 });
 
