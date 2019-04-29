1821053be64a5c864aa1738e51ce2a45271af231
Jor_Poon <jormpoon@gmail.com>
2019-04-29 15:55:16 -0400

    made get status  test for meetings, post status test, and post message test for meetings

---
+++ src/tests/meetings.test.js
@@ -0,0 +1,48 @@
+const request = require('supertest');
+const server = require('../server');
+const Meetings = require('../database/helpers/meetings');
+
+const MEETING_API_URL = '/api/meetings';
+
+afterEach(async () => {
+    await Meetings.truncate();
+})
+
+beforeEach(async () => {
+    await Meetings.truncate();
+})
+
+describe('MEETINGS ROUTER', () => {
+    describe('GET ROUTE /MEETINGS', () => {
+        it('should return status 200 on success', async () => {
+            const res = await request(server).get(MEETING_API_URL);
+            expect(res.status).toEqual(200);
+        });
+    })
+
+    describe('POST ROUTE /MEETINGS', () => {
+        it('should return status 201 on success', async () => {
+            const res = await request(server)
+            .post(MEETING_API_URL)
+            .send({
+                meeting_date: '2016-02-05T03:30:17.883Z',
+                location: 'New York',
+                notes: 'some meeting',
+                rating: Math.floor((Math.random()*5))
+            });
+            expect(res.status).toEqual(201);
+        })
+
+        it('should return a message on success', async () => {
+            const res = await request(server)
+            .post(MEETING_API_URL)
+            .send({
+                meeting_date: '2016-02-05T03:30:17.883Z',
+                location: 'New York',
+                notes: 'some meeting',
+                rating: Math.floor((Math.random()*5))
+            });
+            expect(res.body).toEqual({message:'Meeting has been created'})
+        })
+    })
+})
