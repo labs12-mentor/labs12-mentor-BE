54738ec16f7d178e49e38920607d7fb965d2022f
Jor_Poon <jormpoon@gmail.com>
2019-04-30 11:44:15 -0400

    added 4 tests , success without zip, success without application answers, status 404 without lambsa week and error without lambda week

--- src/tests/menteeProfiles.test.js
+++ src/tests/menteeProfiles.test.js
@@ -77,7 +77,7 @@ describe("MENTEES ROUTER", () => {
           user_id: 1,
           desired_zip: null,
           lambda_week: 5,
-          interests: "",
+          interests: "bball",
           application_answers: "yes",
           wanted_mentor_id: 6
         });
@@ -89,16 +89,98 @@ describe("MENTEES ROUTER", () => {
         .post(MENTEE_API_URL)
         .send({
           user_id: 1,
-          desired_zip: null,
+          desired_zip: 12132,
           lambda_week: 5,
-          interests: "",
+          interests: "bball",
           application_answers: "",
           wanted_mentor_id: 6
         });
       expect(res.status).toEqual(201);
     });
 
-    it("should return message on success",)
+    it("should return message on success", async () => {
+      const res = await request(server)
+       .post(MENTEE_API_URL)
+       .send({
+        user_id: 1,
+        desired_zip: null,
+        lambda_week: 5,
+        interests: "bball",
+        application_answers: "yes",
+        wanted_mentor_id: 6
+       })
+       expect(res.body).toEqual({message: "Mentee profile has been created"})
+    })
+
+    it("should return message on success without interests", async () => {
+      const res = await request(server)
+       .post(MENTEE_API_URL)
+       .send({
+        user_id: 1,
+        desired_zip: 12345,
+        lambda_week: 5,
+        interests: "",
+        application_answers: "yes",
+        wanted_mentor_id: 6
+       });
+       expect(res.body).toEqual({message: "Mentee profile has been created"})
+    })
+
+    it("should return message on success without desired zip", async () => {
+      const res = await request(server)
+      .post(MENTEE_API_URL)
+      .send({
+       user_id: 1,
+       desired_zip: null,
+       lambda_week: 5,
+       interests: "bball",
+       application_answers: "yes",
+       wanted_mentor_id: 6
+      });
+      expect(res.body).toEqual({message: "Mentee profile has been created"})
+    })
+
+    it("should return message on success without application answers", async () => {
+      const res = await request(server)
+      .post(MENTEE_API_URL)
+      .send({
+       user_id: 1,
+       desired_zip: 12134,
+       lambda_week: 5,
+       interests: "bball",
+       application_answers: "",
+       wanted_mentor_id: 6
+      });
+      expect(res.body).toEqual({message: "Mentee profile has been created"})
+    })
+
+    it("should return status 404 on fail without lambda_week", async () => {
+      const res = await request(server)
+      .post(MENTEE_API_URL)
+      .send({
+       user_id: 1,
+       desired_zip: 12134,
+       lambda_week: null,
+       interests: "bball",
+       application_answers: "hey",
+       wanted_mentor_id: 6
+      });
+      expect(res.status).toEqual(404)
+    })
+
+    it("should return error on fail without lambda week", async () => {
+      const res = await request(server)
+      .post(MENTEE_API_URL)
+      .send({
+       user_id: 1,
+       desired_zip: 12134,
+       lambda_week: 5,
+       interests: "bball",
+       application_answers: "",
+       wanted_mentor_id: 6
+      });
+      expect(res.body).toEqual({error: "Please provide the required fields"})
+    })
   });
 });
 
