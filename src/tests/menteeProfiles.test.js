1b78951f1c0c754118a8e9e152ef3826cf83937e
Jor_Poon <jormpoon@gmail.com>
2019-04-30 11:56:20 -0400

    created 2 tests for GET/MENTEE/:id >> status 200 on success and return specified meeting

--- src/tests/menteeProfiles.test.js
+++ src/tests/menteeProfiles.test.js
@@ -2,27 +2,27 @@ const request = require("supertest");
 const server = require("../server");
 const Mentees = require("../database/helpers/menteeProfiles");
 
-const MENTEE_API_URL = "/api/menteeProfiles"
+const MENTEE_API_URL = "/api/menteeProfiles";
 
 const sampleProfile = {
-    user_id: 1,
-    desired_zip: 11009,
-    lambda_week: 5,
-    interests: "basketball",
-    application_answers: "yes",
-    wanted_mentor_id: 6
-}
+  user_id: 1,
+  desired_zip: 11009,
+  lambda_week: 5,
+  interests: "basketball",
+  application_answers: "yes",
+  wanted_mentor_id: 6
+};
 
 afterEach(async () => {
-    await Mentees.truncate();
-  });
-  
-  beforeEach(async () => {
-    await Mentees.truncate();
-  });
+  await Mentees.truncate();
+});
+
+beforeEach(async () => {
+  await Mentees.truncate();
+});
 
 async function createMentee() {
-    return await request(server)
+  return await request(server)
     .post(MENTEE_API_URL)
     .send(sampleProfile);
 }
@@ -100,87 +100,112 @@ describe("MENTEES ROUTER", () => {
 
     it("should return message on success", async () => {
       const res = await request(server)
-       .post(MENTEE_API_URL)
-       .send({
-        user_id: 1,
-        desired_zip: null,
-        lambda_week: 5,
-        interests: "bball",
-        application_answers: "yes",
-        wanted_mentor_id: 6
-       })
-       expect(res.body).toEqual({message: "Mentee profile has been created"})
-    })
+        .post(MENTEE_API_URL)
+        .send({
+          user_id: 1,
+          desired_zip: null,
+          lambda_week: 5,
+          interests: "bball",
+          application_answers: "yes",
+          wanted_mentor_id: 6
+        });
+      expect(res.body).toEqual({ message: "Mentee profile has been created" });
+    });
 
     it("should return message on success without interests", async () => {
       const res = await request(server)
-       .post(MENTEE_API_URL)
-       .send({
-        user_id: 1,
-        desired_zip: 12345,
-        lambda_week: 5,
-        interests: "",
-        application_answers: "yes",
-        wanted_mentor_id: 6
-       });
-       expect(res.body).toEqual({message: "Mentee profile has been created"})
-    })
+        .post(MENTEE_API_URL)
+        .send({
+          user_id: 1,
+          desired_zip: 12345,
+          lambda_week: 5,
+          interests: "",
+          application_answers: "yes",
+          wanted_mentor_id: 6
+        });
+      expect(res.body).toEqual({ message: "Mentee profile has been created" });
+    });
 
     it("should return message on success without desired zip", async () => {
       const res = await request(server)
-      .post(MENTEE_API_URL)
-      .send({
-       user_id: 1,
-       desired_zip: null,
-       lambda_week: 5,
-       interests: "bball",
-       application_answers: "yes",
-       wanted_mentor_id: 6
-      });
-      expect(res.body).toEqual({message: "Mentee profile has been created"})
-    })
+        .post(MENTEE_API_URL)
+        .send({
+          user_id: 1,
+          desired_zip: null,
+          lambda_week: 5,
+          interests: "bball",
+          application_answers: "yes",
+          wanted_mentor_id: 6
+        });
+      expect(res.body).toEqual({ message: "Mentee profile has been created" });
+    });
 
     it("should return message on success without application answers", async () => {
       const res = await request(server)
-      .post(MENTEE_API_URL)
-      .send({
-       user_id: 1,
-       desired_zip: 12134,
-       lambda_week: 5,
-       interests: "bball",
-       application_answers: "",
-       wanted_mentor_id: 6
-      });
-      expect(res.body).toEqual({message: "Mentee profile has been created"})
-    })
+        .post(MENTEE_API_URL)
+        .send({
+          user_id: 1,
+          desired_zip: 12134,
+          lambda_week: 5,
+          interests: "bball",
+          application_answers: "",
+          wanted_mentor_id: 6
+        });
+      expect(res.body).toEqual({ message: "Mentee profile has been created" });
+    });
 
     it("should return status 404 on fail without lambda_week", async () => {
       const res = await request(server)
-      .post(MENTEE_API_URL)
-      .send({
-       user_id: 1,
-       desired_zip: 12134,
-       lambda_week: null,
-       interests: "bball",
-       application_answers: "hey",
-       wanted_mentor_id: 6
-      });
-      expect(res.status).toEqual(404)
-    })
+        .post(MENTEE_API_URL)
+        .send({
+          user_id: 1,
+          desired_zip: 12134,
+          lambda_week: null,
+          interests: "bball",
+          application_answers: "hey",
+          wanted_mentor_id: 6
+        });
+      expect(res.status).toEqual(404);
+    });
 
     it("should return error on fail without lambda week", async () => {
       const res = await request(server)
-      .post(MENTEE_API_URL)
-      .send({
-       user_id: 1,
-       desired_zip: 12134,
-       lambda_week: 5,
-       interests: "bball",
-       application_answers: "",
-       wanted_mentor_id: 6
-      });
-      expect(res.body).toEqual({error: "Please provide the required fields"})
-    })
+        .post(MENTEE_API_URL)
+        .send({
+          user_id: 1,
+          desired_zip: 12134,
+          lambda_week: 5,
+          interests: "bball",
+          application_answers: "",
+          wanted_mentor_id: 6
+        });
+      expect(res.body).toEqual({ error: "Please provide the required fields" });
+    });
   });
-});
 
+  describe("GET ROUTE /MENTEES/:id", () => {
+    it("should return status 200 on success", async () => {
+      await createMentee();
+
+      const res = await request(server).get(`${MENTEE_API_URL}/1`);
+
+      expect(res.status).toEqual(200);
+    })
+
+    it("should return specified meeting", () => {
+      await createMentee();
+
+      const res = await request(server).get(`${MENTEE_API_URL}/1`);
+
+      expect(res.body).toEqual({
+        id: 1,
+        user_id: 1,
+        desired_zip: 11009,
+        lambda_week: 5,
+        interests: "basketball",
+        application_answers: "yes",
+        wanted_mentor_id: 6
+      });
+    })
+  })
+});
