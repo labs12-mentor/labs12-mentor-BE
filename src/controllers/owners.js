a8f6eb1f1e8d2788dfacee344ec6d89519e3f8de
Łukasz Siatka <lukasz@lukaszsiatka.pl>
2019-05-01 15:12:22 -0700

    added get owner method

--- src/controllers/owners.js
+++ src/controllers/owners.js
@@ -13,17 +13,26 @@ async function getOwners(req, res){
         const owners = await Owners.getOwners();
         return await res.status(200).json(owners);
     } catch(error) {
-        return await res.status(500).json({ error: 'Could not fetch owners!' });
+        return await res.status(500).json({ error: 'Cannot fetch owners!' });
     }
 }
 
 async function addOwner(req, res){
-    res.status(200).json({ message: 'add owner API OK' });
+    try {
+
+    } catch(error) {
+        return await res.status(500).json({ error: 'Cannot add new owner!' });
+    }
 }
 
 async function getOwner(req, res){
-    const owner = await Owners.getOwnerById(req.params.id);
-    res.status(200).json(owner);
+    try {
+        const owner = await Owners.getOwnerById(req.params.id);
+        if(owner.length === 0) return await res.status(404).json({ error: 'Owner not found!' });
+        return await res.status(200).json(owner);
+    } catch(error) {
+        return await res.status(500).json({ error: 'Cannot fetch owner!' });
+    }
 }
 
 async function updateOwner(req, res){
