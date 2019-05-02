e5d1f03abdd911ebe510efdc8039a5cbfb9a0ebd
Łukasz Siatka <lukasz@lukaszsiatka.pl>
2019-05-01 21:02:56 -0700

    updated auth validator

--- src/validators/auth.js
+++ src/validators/auth.js
@@ -1,47 +1,26 @@
 module.exports = {
     validateUser,
     validateCredentials,
-    validateOwner,
-    validateAdministrator
+    validateOrganization
 }
 
 async function validateUser(userData){
     if(
-        !userData.username
+        !userData.email
         || !userData.password
-        || !userData.first_name
-        || !userData.last_name
-        || !userData.email
     ) return false;
 
     return true;
 }
 
 async function validateCredentials(userData){
-    if(!userData.username || !userData.password) return false;
+    if(!userData.email || !userData.password) return false;
 
     return true;
 }
 
-async function validateOwner(userData){
-    if(
-        !userData.username
-        || !userData.password
-        || !userData.email
-    ) return false;
-    
-    return true;
-}
+async function validateOrganization(organizationData){
+    if(!organizationData.name) return false;
 
-async function validateAdministrator(userData){
-    if(
-        !userData.username
-        || !userData.password
-        || !userData.first_name
-        || !userData.last_name
-        || !userData.email
-        || !userData.company_name
-    ) return false;
-    
     return true;
 }
