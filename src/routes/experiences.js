6cf38a4bbe7dc943af9be998313ac0c1e9ead614
Łukasz Siatka <lukasz@lukaszsiatka.pl>
2019-05-02 04:12:36 -0700

    added auth and authorization middleware to experiences router

--- src/routes/experiences.js
+++ src/routes/experiences.js
@@ -1,5 +1,7 @@
 const router = require('express').Router();
 const { experiencesController } = require('../controllers');
+const authenticate = require('../middleware/authenticate');
+const authorize = require('../middleware/authorize');
 
 router.route('/')
     .get(experiencesController.getExperiences)
