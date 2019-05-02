02ccdad9d7a029e2e30cf95d6b188b2f97e595eb
Łukasz Siatka <lukasz@lukaszsiatka.pl>
2019-05-01 23:06:24 -0700

    updated users controller - added actions to update, delete and remove user

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -31,7 +31,12 @@ async function getCurrentUser(req, res){
 
 async function getUser(req, res){
     try {
+        const current_user = await Users.getUserById(req.user.id);
         const user = await Users.getUserById(req.params.id);
+
+        if(current_user.role !== 'ADMINISTRATOR' && current_user.organization_id !== user.organization_id){
+            return await res.status(403).json({ error: 'Cannot find this user!' });
+        }
 
         if(user === undefined || user.deleted) return await res.status(404).json({ error: 'User not found!' });
         
@@ -44,24 +49,60 @@ async function getUser(req, res){
 async function updateUser(req, res){
     try {
         const current_user = await Users.getUserById(req.user.id);
-        const user = await UIEvent.getUserById(req.params.id);
+        const user = await Users.getUserById(req.params.id);
+
+        if(current_user.role !== 'ADMINISTRATOR'){
+            if(!((current_user.role === 'OWNER' || current_user.role === 'MANAGER') && current_user.organization_id === user.organization_id)||(current_user.role === 'USER' && current_user.id !== user.id)){
+                return await res.status(403).json({ error: 'Cannot find this user!' });
+            }
+        }
 
         if(user === undefined || user.deleted) return await res.status(404).json({ error: 'User not found!' });
-        
-        if((current_user.role !== 'ADMINISTRATOR' && current_user.role !== 'OWNER' && current_user.role !== 'MANAGER') && current_user.id !== user.id){
-            return await res.status(403).json({ error: 'This profile is not yours!' });
-        }
-        
-        return await res.status(200).json(user);
+        await Users.updateUser(req.params.id, {
+            ...user,
+            email: req.body.email,
+            first_name: req.body.first_name,
+            last_name: req.body.last_name,
+            street: req.body.street,
+            city: req.body.city,
+            state: req.body.state,
+            zipcode: req.body.zipcode,
+            country: req.body.country
+        });
+
+        return await res.status(200).json({ message: 'User successfully updated!' });
     } catch(error) {
         return await res.status(500).json({ error: error.message });
     }
 }
 
 async function deleteUser(req, res){
-    res.status(200).json({ message: 'delete user API OK' });
+    try {
+        const current_user = await Users.getUserById(req.user.id);
+        const user = await Users.getUserById(req.params.id);
+
+        if(current_user.role !== 'ADMINISTRATOR'){
+            if(!((current_user.role === 'OWNER' || current_user.role === 'MANAGER') && current_user.organization_id === user.organization_id)||(current_user.role === 'USER' && current_user.id !== user.id)){
+                return await res.status(403).json({ error: 'Cannot find this user!' });
+            }
+        }
+
+        if(user === undefined || user.deleted) return await res.status(404).json({ error: 'User not found!' });
+        await Users.deleteUser(req.params.id);
+
+        return await res.status(200).json({ message: 'User successfully deleted!' });
+    } catch(error) {
+        return await res.status(500).json({ error: error.message });
+    }
 }
 
 async function removeUser(req, res){
-    res.status(200).json({ message: 'remove user API OK' });
+    try {
+        const user = await Users.getUserById(req.params.id);
+        if(user === undefined) return await res.status(404).json({ error: 'User not found!' });
+        await Users.removeUser(req.params.id);
+        return await res.status(200).json({ message: 'User successfully removed!' });
+    } catch(error) {
+        return await res.status(500).json({ error: error.message });
+    }
 }
