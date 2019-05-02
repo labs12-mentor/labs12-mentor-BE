65e232d32c03e5e1935292dd7c4e301be9706bff
Łukasz Siatka <lukasz@lukaszsiatka.pl>
2019-05-01 21:06:01 -0700

    updated routes

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -5,18 +5,6 @@ router.route('/login')
     .post(authController.loginUser);
 
 router.route('/register')
-    .post(authController.registerUser);
-
-router.route('/admin/login')
-    .post(authController.loginAdministrator);
-
-router.route('/admin/register')
-    .post(authController.registerAdministrator);
-
-router.route('/owner/login')
-    .post(authController.loginOwner);
-
-router.route('/owner/register')
-    .post(authController.registerOwner);
+    .post(authController.register);
 
 module.exports = router;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,31 +1,27 @@
 const router = require('express').Router();
 const { apiController } = require('../controllers');
 
-const administrators = require('./administrators');
 const auth = require('./auth');
-const experiences = require('./experiences');
-const matches = require('./matches');
-const meetings = require('./meetings');
-const menteeProfiles = require('./menteeProfiles');
-const mentorProfiles = require('./mentorProfiles');
-const notifications = require('./notifications');
+// const experiences = require('./experiences');
+// const matches = require('./matches');
+// const meetings = require('./meetings');
+// const menteeProfiles = require('./menteeProfiles');
+// const mentorProfiles = require('./mentorProfiles');
+// const notifications = require('./notifications');
 const organizations = require('./organizations');
-const owners = require('./owners');
 const users = require('./users');
 
 router.route('/')
     .get(apiController.rootRoute);
 
-router.use('/administrators', administrators);
 router.use('/auth', auth);
-router.use('/experiences', experiences);
-router.use('/matches', matches);
-router.use('/meetings', meetings);
-router.use('/mentees', menteeProfiles);
-router.use('/mentors', mentorProfiles);
-router.use('/notifications', notifications);
+// router.use('/experiences', experiences);
+// router.use('/matches', matches);
+// router.use('/meetings', meetings);
+// router.use('/mentees', menteeProfiles);
+// router.use('/mentors', mentorProfiles);
+// router.use('/notifications', notifications);
 router.use('/organizations', organizations);
-router.use('/owners', owners);
 router.use('/users', users);
 
 module.exports = router;
