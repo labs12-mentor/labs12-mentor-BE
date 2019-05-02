65014b610a238aebaed2f157e67295f751bd87e3
Łukasz Siatka <lukasz@lukaszsiatka.pl>
2019-05-02 04:12:00 -0700

    updated index router

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,12 +2,12 @@ const router = require('express').Router();
 const { apiController } = require('../controllers');
 
 const auth = require('./auth');
-// const experiences = require('./experiences');
-// const matches = require('./matches');
-// const meetings = require('./meetings');
-// const menteeProfiles = require('./menteeProfiles');
-// const mentorProfiles = require('./mentorProfiles');
-// const notifications = require('./notifications');
+const experiences = require('./experiences');
+const matches = require('./matches');
+const meetings = require('./meetings');
+const menteeProfiles = require('./menteeProfiles');
+const mentorProfiles = require('./mentorProfiles');
+const notifications = require('./notifications');
 const organizations = require('./organizations');
 const users = require('./users');
 
@@ -15,12 +15,12 @@ router.route('/')
     .get(apiController.rootRoute);
 
 router.use('/auth', auth);
-// router.use('/experiences', experiences);
-// router.use('/matches', matches);
-// router.use('/meetings', meetings);
-// router.use('/mentees', menteeProfiles);
-// router.use('/mentors', mentorProfiles);
-// router.use('/notifications', notifications);
+router.use('/experiences', experiences);
+router.use('/matches', matches);
+router.use('/meetings', meetings);
+router.use('/mentees', menteeProfiles);
+router.use('/mentors', mentorProfiles);
+router.use('/notifications', notifications);
 router.use('/organizations', organizations);
 router.use('/users', users);
 
