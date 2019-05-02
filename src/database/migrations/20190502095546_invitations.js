6dddb13bfbf3e1665c372db35f0bc5ea156a14a7
Łukasz Siatka <lukasz@lukaszsiatka.pl>
2019-05-02 10:00:02 -0700

    added the invitations migration

--- src/database/migrations/20190502095546_invitations.js
+++ src/database/migrations/20190502095546_invitations.js
@@ -1,8 +1,38 @@
+const { dropTable } = require('../helpers/db');
 
 exports.up = function(knex, Promise) {
-  
+    return knex.schema.createTable('meetings', table => {
+        table
+            .increments('id');
+
+        table
+            .integer('organization_id')
+            .unsigned()
+            .references('id')
+            .inTable('organizations')
+            .notNullable()
+            .onUpdate('CASCADE')
+            .onDelete('CASCADE');
+        
+        table
+            .integer('user_id')
+            .unsigned()
+            .references('id')
+            .inTable('users')
+            .notNullable()
+            .onUpdate('CASCADE')
+            .onDelete('CASCADE');
+        
+        table
+            .string('role')
+            .defaultTo('USER')
+            .notNullable();
+        
+        table
+            .boolean('deleted')
+            .defaultTo(false)
+            .notNullable();
+    });
 };
 
-exports.down = function(knex, Promise) {
-  
-};
+exports.down = dropTable('meetings');
