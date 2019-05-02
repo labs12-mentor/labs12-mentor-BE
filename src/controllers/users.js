105b8345ca9a3682d883d59cb5a534da83b05868
Łukasz Siatka <lukasz@lukaszsiatka.pl>
2019-05-01 22:41:01 -0700

    updated users controller - added get current user method

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -18,19 +18,22 @@ async function getAllUsers(req, res){
 }
 
 async function getCurrentUser(req, res){
-    const user = await Users.getUserById(req.user.id);
-    res.status(200).json(user);
-}
-
-async function getUser(req, res){
     try {
         const user = await Users.getUserById(req.user.id);
 
         if(user === undefined || user.deleted) return await res.status(404).json({ error: 'User not found!' });
+        
+        return await res.status(200).json(user);
+    } catch(error) {
+        return await res.status(500).json({ error: error.message });
+    }
+}
 
-        if(user.role !== 'ADMINISTRATOR' && user.id !== req.params.id){
-            return await res.status(403).json({ error: 'This profile is not yours!' });
-        }
+async function getUser(req, res){
+    try {
+        const user = await Users.getUserById(req.params.id);
+
+        if(user === undefined || user.deleted) return await res.status(404).json({ error: 'User not found!' });
         
         return await res.status(200).json(user);
     } catch(error) {
@@ -39,7 +42,20 @@ async function getUser(req, res){
 }
 
 async function updateUser(req, res){
-    res.status(200).json({ message: 'update user API OK' });
+    try {
+        const current_user = await Users.getUserById(req.user.id);
+        const user = await UIEvent.getUserById(req.params.id);
+
+        if(user === undefined || user.deleted) return await res.status(404).json({ error: 'User not found!' });
+        
+        if((current_user.role !== 'ADMINISTRATOR' && current_user.role !== 'OWNER' && current_user.role !== 'MANAGER') && current_user.id !== user.id){
+            return await res.status(403).json({ error: 'This profile is not yours!' });
+        }
+        
+        return await res.status(200).json(user);
+    } catch(error) {
+        return await res.status(500).json({ error: error.message });
+    }
 }
 
 async function deleteUser(req, res){
