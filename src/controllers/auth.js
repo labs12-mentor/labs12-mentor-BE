ac4361393a02f92be6a1163efe189d700a910719
Łukasz Siatka <lukasz@lukaszsiatka.pl>
2019-05-01 21:04:15 -0700

    updated auth controller to match current schema

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,30 +1,21 @@
 module.exports = {
     loginUser,
-    registerUser,
-    loginAdministrator,
-    registerAdministrator,
-    loginOwner,
-    registerOwner
+    register
 }
 
 const Users = require('../database/helpers/users');
-const Owners = require('../database/helpers/owners');
-const Administrators = require('../database/helpers/administrators');
+const Organizations = require('../database/helpers/organizations');
 const bcrypt = require('bcryptjs');
 const generateToken = require('../middleware/generateToken');
 const { authValidator } = require('../validators');
 const passport = require('passport');
 
 async function loginUser(req, res){
-    const {
-        username,
+    const userData = {
+        email,
         password
     } = req.body;
 
-    const userData = {
-        username,
-        password
-    }
     const validCredentials = await authValidator.validateCredentials(userData);
     if(!validCredentials){
         return await res
@@ -37,26 +28,15 @@ async function loginUser(req, res){
             req.login(user, { session: false }, async (err) => {
                 if(err) throw new Error('Cannot log in!');
                 const token = await generateToken(user);
+                const {password, ...userWithoutPassword} = user;
                 return res
                     .status(200)
                     .json({
-                        user,
+                        ...userWithoutPassword,
                         token
                     });
             })
         })(req, res);
-        
-        // const user = await Users.getUserByUsername(userData.username);
-        // if(user && !user.deleted && bcrypt.compareSync(userData.password, user.password)){
-        //     const token = await generateToken(user);
-        //     return await res
-        //         .status(200)
-        //         .json({ token });
-        // } else {
-        //     return await res
-        //         .status(404)
-        //         .json({ error: 'No user found!' });
-        // }
     } catch(error) {
         return await res
             .status(500)
@@ -64,197 +44,60 @@ async function loginUser(req, res){
     }
 }
 
-async function registerUser(req, res){
+async function register(req, res){
     const {
-        username,
-        password,
-        first_name,
-        last_name,
-        email,
-        country,
-        state,
-        city,
-        zipcode,
-        organization_id
+        organization_name,
+        organization_description,
+        organization_logo,
+        user_email,
+        user_password,
+        user_first_name,
+        user_last_name
     } = req.body;
 
-    let userData = {
-        username,
-        password,
-        first_name,
-        last_name,
-        email,
-        country,
-        state,
-        city,
-        zipcode,
-        organization_id
+    let organizationData = {
+        name: organization_name,
+        description: organization_description,
+        logo: organization_logo
     }
 
+    let userData = {
+        email: user_email,
+        password: user_password,
+        first_name: user_first_name,
+        last_name: user_last_name,
+        organization_id: -1
+    }
+
+    const validOrganization = await authValidator.validateOrganization(organizationData);
     const validUser = await authValidator.validateUser(userData);
-    if(!validUser){
+    if(!validUser || !validOrganization){
         return await res
             .status(400)
-            .json({ error: 'Cannot register user!' });
+            .json({ error: 'Cannot register organization or user - missing required fields!' });
     }
+
+    if(await Users.getUserByEmail(userData.email) || await Organizations.getOrganizationByName(organizationData.name)){
+        return await res
+            .status(400)
+            .json({ error: 'User or organization already exists!' });
+    }
+
     try {
+        const org = await Organizations.insertOrganization(organizationData);
+
         let hashedPassword = bcrypt.hashSync(userData.password, 10);
         userData.password = hashedPassword;
-        const user = await Users.getUserByUsername(userData.username);
-        if(user !== undefined) throw new Error('User already registered!');
-        await Users.insertUser(userData);
+
+        await Users.insertUser({
+            ...userData,
+            organization_id: org.id,
+            role: 'OWNER'
+        });
+
         return res
             .status(201)
             .json({ message: 'User successfully registered!' });
-    } catch(error) {
-        return await res
-            .status(500)
-            .json({ error: error.message });
-    }
-}
-
-async function loginOwner(req, res){
-    const {
-        username,
-        password
-    } = req.body;
-
-    const userData = {
-        username,
-        password
-    }
-
-    const validCredentials = await authValidator.validateCredentials(userData);
-    if(!validCredentials){
-        return await res
-            .status(400)
-            .json({ error: 'Login failed. Wrong credentials!' });
-    }
-    try {
-        const user = await Owners.getOwnerByUsername(userData.username);
-        if(user && !user.deleted && bcrypt.compareSync(userData.password, user.password)){
-            const token = await generateToken(user);
-            return await res
-                .status(200)
-                .json({ token });
-        } else {
-            return await res
-                .status(404)
-                .json({ error: 'No owner found!' });
-        }
-    } catch(error) {
-        return await res
-            .status(500)
-            .json({ error });
-    }
-}
-
-async function registerOwner(req, res){
-    const {
-        username,
-        password,
-        email,
-        company_name
-    } = req.body;
-
-    let userData = {
-        username,
-        password,
-        email,
-        company_name
-    }
-    const checkIfCanRegister = await Owners.checkIfCanRegister();
-    const validOwner = await authValidator.validateOwner(userData);
-    if(!validOwner || !checkIfCanRegister){
-        return await res
-            .status(400)
-            .json({ error: 'Cannot register new owner!' });
-    }
-    try {
-        let hashedPassword = bcrypt.hashSync(userData.password, 10);
-        userData.password = hashedPassword;
-        const user = await Owners.getOwnerByUsername(userData.username);
-        if(user !== undefined) throw new Error('Owner already registered!');
-        await Owners.insertOwner(userData);
-        return res
-            .status(201)
-            .json({ message: 'Owner successfully registered!' });
-    } catch(error) {
-        return await res
-            .status(500)
-            .json({ error: error.message });
-    }
-}
-
-async function loginAdministrator(req, res){
-    const {
-        username,
-        password
-    } = req.body;
-
-    const userData = {
-        username,
-        password
-    }
-    
-    const validCredentials = await authValidator.validateCredentials(userData);
-    if(!validCredentials){
-        return await res
-            .status(400)
-            .json({ error: 'Login failed. Wrong credentials!' });
-    }
-    try {
-        const user = await Administrators.getAdministratorByUsername(userData.username);
-        if(user && !user.deleted && bcrypt.compareSync(userData.password, user.password)){
-            const token = await generateToken(user);
-            return await res
-                .status(200)
-                .json({ token });
-        } else {
-            return await res
-                .status(404)
-                .json({ error: 'No administrator found!' });
-        }
-    } catch(error) {
-        return await res
-            .status(500)
-            .json({ error });
-    }
-}
-
-async function registerAdministrator(req, res){
-    const {
-        username,
-        password,
-        first_name,
-        last_name,
-        email,
-        company_name
-    } = req.body;
-
-    let userData = {
-        username,
-        password,
-        first_name,
-        last_name,
-        email,
-        company_name
-    }
-    const validAdministrator = await authValidator.validateAdministrator(userData);
-    if(!validAdministrator){
-        return await res
-            .status(400)
-            .json({ error: 'Cannot register new administrator!' });
-    }
-    try {
-        let hashedPassword = bcrypt.hashSync(userData.password, 10);
-        userData.password = hashedPassword;
-        const user = await Administrators.getAdministratorByUsername(userData.username);
-        if(user !== undefined) throw new Error('Administrator already registered!');
-        await Administrators.insertAdministrator(userData);
-        return res
-            .status(201)
-            .json({ message: 'Administrator successfully registered!' });
     } catch(error) {
         return await res
             .status(500)
