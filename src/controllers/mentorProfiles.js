891e408875aa07840e4c716441fc17653d8f20b9
Łukasz Siatka <lukasz@lukaszsiatka.pl>
2019-05-02 05:18:49 -0700

    updated mentor profiles controller

--- src/controllers/mentorProfiles.js
+++ src/controllers/mentorProfiles.js
@@ -7,29 +7,86 @@ module.exports = {
     removeMentorProfile
 }
 const MentorProfiles = require('../database/helpers/mentorProfiles');
+const Users = require('../database/helpers/users');
 
 async function getMentorProfiles(req, res){
-    const mentors = await MentorProfiles.getMentorProfiles();
-    res.status(200).json(mentors);
+    try {
+        // const user = await Users.getUserById(req.user.id);
+        
+        // if(user === undefined){
+        //     return await res.status(403).json({ error: 'You are not authenticated!' });
+        // }
+
+        // let mentors = [];
+        // if(user.role !== 'ADMINISTRATOR'){
+        //     mentors = await MentorProfiles.getMentorProfilesByOrganization(user.organization_id);
+        // }else{
+        const mentors = await MentorProfiles.getMentorProfiles();
+        // }
+
+        // if(mentors === undefined){
+        //     mentors = [];
+        // }
+
+        return await res.status(200).json(mentors);
+    } catch(error) {
+        return await res.status(500).json({ error: error.message });
+    }
 }
 
 async function addMentorProfile(req, res){
-    res.status(200).json({ message: 'add mentor profile API OK' });
+    try {
+        const mentorProfile = { user_id } = req.body;
+        const mentor = await MentorProfiles.getMentorProfileById(req.params.id);
+        if(mentor !== undefined) return await res.status(404).json({ error: 'Mentor already exists!' });
+
+        await MentorProfiles.insertMentorProfile(mentorProfile);
+        return await res.status(200).json({ message: 'Successfully added a mentor!' });
+    } catch(error) {
+        return await res.status(500).json({ error: error.message });
+    }
 }
 
 async function getMentorProfile(req, res){
-    const mentor = await MentorProfiles.getMentorProfileById(req.params.id);
-    res.status(200).json(mentor);
+    try {
+        const mentor = await MentorProfiles.getMentorProfileById(req.params.id);
+        if(mentor === undefined || mentor.deleted) return await res.status(404).json({ error: 'Mentor not found!' });
+        return await res.status(200).json(mentor);
+    } catch(error) {
+        return await res.status(500).json({ error: error.message });
+    }
 }
 
 async function updateMentorProfile(req, res){
-    res.status(200).json({ message: 'update mentor profile API OK' });
+    try {
+        const mentorProfile = { user_id } = req.body;
+        const mentor = await MentorProfiles.getMentorProfileById(req.params.id);
+        if(mentor === undefined || mentor.deleted) return await res.status(404).json({ error: 'Mentor not found!' });
+        await MentorProfiles.updateMentorProfile(req.params.id, mentorProfile);
+        return await res.status(200).json({ message: 'Successfully updated a mentor!' });
+    } catch(error) {
+        return await res.status(500).json({ error: error.message });
+    }
 }
 
 async function deleteMentorProfile(req, res){
-    res.status(200).json({ message: 'delete mentor profile API OK' });
+    try {
+        const mentor = await MentorProfiles.getMentorProfileById(req.params.id);
+        if(mentor === undefined || mentor.deleted) return await res.status(404).json({ error: 'Mentor not found!' });
+        await MentorProfiles.deleteMentorProfile(req.params.id);
+        return await res.status(200).json({ message: 'Successfully deleted a mentor!' });
+    } catch(error) {
+        return await res.status(500).json({ error: error.message });
+    }
 }
 
 async function removeMentorProfile(req, res){
-    res.status(200).json({ message: 'remove mentor profile API OK' });
+    try {
+        const mentor = await MentorProfiles.getMentorProfileById(req.params.id);
+        if(mentor === undefined) return await res.status(404).json({ error: 'Mentor not found!' });
+        await MentorProfiles.removeMentorProfile(req.params.id);
+        return await res.status(200).json({ message: 'Successfully removed a mentor!' });
+    } catch(error) {
+        return await res.status(500).json({ error: error.message });
+    }
 }
