94be43fd1d18f8c0cb24a45d63510485c955e7c5
Łukasz Siatka <lukasz@lukaszsiatka.pl>
2019-05-01 21:59:31 -0700

    updated auth middleware - returns

--- src/middleware/authorize.js
+++ src/middleware/authorize.js
@@ -2,13 +2,13 @@ const Users = require('../database/helpers/users');
 
 module.exports = (roles) => async (req, res, next) => {
     if(roles.includes('ALL')){
-        next();
+        return next();
     } else {
         const { role } = await Users.getUserById(req.user.id);
         if(!roles.includes(role)){
-            res.status(403).json({ error: 'Not authorized' });
+            return res.status(403).json({ error: 'Not authorized' });
         } else {
-            next();
+            return next();
         }
     }
 }
