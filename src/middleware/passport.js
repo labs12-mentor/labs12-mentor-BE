2494b8c164f9e0233fc1f0eb34d83ed8d5618de8
Łukasz Siatka <lukasz@lukaszsiatka.pl>
2019-05-03 15:11:34 -0700

    updated middleware for passport and sessions

--- src/middleware/passport.js
+++ src/middleware/passport.js
@@ -2,15 +2,27 @@ require('dotenv').config();
 const passport = require('passport');
 const passportJWT = require('passport-jwt');
 const LocalStrategy = require('passport-local').Strategy;
+const GitHubStrategy = require('passport-github').Strategy;
 const JWTStrategy = passportJWT.Strategy;
 const Users = require('../database/helpers/users');
+const Invitations = require('../database/helpers/invitations');
 const bcrypt = require('bcryptjs');
 const ExtractJWT  = passportJWT.ExtractJwt;
+const request = require('superagent');
+
+passport.serializeUser(function(user, done){
+    done(null, user);
+});
+
+passport.deserializeUser(function(user, done){
+    done(null, user);
+});
 
 passport.use(new LocalStrategy({
         usernameField: 'email',
         passwordField: 'password'
     }, (email, password, cb) => {
+        console.log(email, password);
         return Users.getUserByEmail(email)
             .then(user => {
                 if(bcrypt.compareSync(password, user.password)){
@@ -23,7 +35,7 @@ passport.use(new LocalStrategy({
 ));
 
 passport.use(new JWTStrategy({
-        jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
+        jwtFromRequest: ExtractJWT.fromAuthHeaderWithScheme("Authorization"),
         secretOrKey: process.env.SECRET_KEY
     }, (jwtPayload, cb) => {
         return Users.getUserById(jwtPayload.id)
@@ -36,6 +48,54 @@ passport.use(new JWTStrategy({
             .catch(err => {
                 return cb(err);
             });
+    }
+));
+
+passport.use(new GitHubStrategy({
+        clientID: process.env.GITHUB_CLIENT_ID,
+        clientSecret: process.env.GITHUB_CLIENT_SECRET,
+        callbackURL: "http://localhost:5000/api/auth/github/callback",
+        passReqToCallback: true,
+        scope: [ 'user:email' ]
+    }, (req, accessToken, refreshToken, profile, cb) => {
+        Users.getUserByGithubId(profile.id, async (user) => {
+            if (!user) {
+                if(req.query.state.length === 0){
+                    return cb("Cannot register the user - no organization ID provided!");
+                }else{
+                    const invitationID = JSON.parse(req.query.state).invitation_id;
+                    const invitationData = {
+                        organization_id,
+                        role
+                    } = await Invitations.getInvitationById(invitationID);
+                    
+                    request
+                        .get('https://api.github.com/user/emails')
+                        .set('Accept-Language', 'en-us')
+                        .set('Accept', 'application/json')
+                        .set('Authorization', `token ${accessToken}`)
+                        .set('Accept-Encoding', 'gzip, deflate')
+                        .then(async (response) => {
+                            const userData = {
+                                email: response.body.filter(elem => elem.primary)[0].email,
+                                password: accessToken,
+                                github_id: profile.id,
+                                github_token: accessToken,
+                                role: invitationData.role,
+                                organization_id: invitationData.organization_id
+                            }
+                            await Users.insertUser(userData)
+                                .then(async (result) => {
+                                    const user = await Users.getUserById(result.id);
+                                    return cb(null, user);
+                                })
+                                .catch(err => cb(err));
+                            })
+                        .catch(err => cb(err));
+                }
+            }
+            return cb(null, user);
+        });
     }
 ));
 
