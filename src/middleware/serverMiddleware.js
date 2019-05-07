f2e6a4bdfd682e2af966fa29647b2e53dbe8a45f
Łukasz Siatka <lukasz@lukaszsiatka.pl>
2019-05-06 17:31:12 -0700

    added session middleware

--- src/middleware/serverMiddleware.js
+++ src/middleware/serverMiddleware.js
@@ -1,8 +1,10 @@
+require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
 const helmet = require('helmet');
 const cors = require('cors');
 const passport = require('passport');
+const session = require('express-session');
 
 module.exports = server => {
     server.use(express.json());
@@ -10,6 +12,11 @@ module.exports = server => {
     server.use(morgan('dev'));
     server.use(helmet());
     server.use(cors());
+    server.use(session({ 
+        secret: process.env.SESSION_SECRET, 
+        resave: true, 
+        saveUninitialized: true
+    }));
     server.use(passport.initialize());
     server.use(passport.session());
 };
