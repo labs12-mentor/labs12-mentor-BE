703818f783bf2cab7a5d3bd8dfc32e564974c113
Łukasz Siatka <lukasz@lukaszsiatka.pl>
2019-05-13 17:42:56 -0700

    updated db migrations for mentees and mentors

--- src/database/migrations/20190501192222_mentorprofiles.js
+++ src/database/migrations/20190501192222_mentorprofiles.js
@@ -1,22 +1,23 @@
-const { dropTable } = require("../helpers/db");
+const { dropTable } = require('../helpers/db');
 
 exports.up = function(knex, Promise) {
-  return knex.schema.createTable("mentorprofiles", table => {
-    table.increments("id");
+    return knex.schema.createTable('mentorprofiles', (table) => {
+        table.increments('id');
+        table.string('status').notNullable();
 
-    table
-      .integer("user_id")
-      .unsigned()
-      .references("id")
-      .inTable("users")
-      .onUpdate("CASCADE")
-      .onDelete("CASCADE");
+        table
+            .integer('user_id')
+            .unsigned()
+            .references('id')
+            .inTable('users')
+            .onUpdate('CASCADE')
+            .onDelete('CASCADE');
 
-    table
-      .boolean("deleted")
-      .defaultTo(false)
-      .notNullable();
-  });
+        table
+            .boolean('deleted')
+            .defaultTo(false)
+            .notNullable();
+    });
 };
 
-exports.down = dropTable("mentorprofiles");
+exports.down = dropTable('mentorprofiles');
