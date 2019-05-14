c450744a9b777b09f79e1d91adacc53dc3776de4
Łukasz Siatka <lukasz@lukaszsiatka.pl>
2019-05-13 17:43:09 -0700

    updated seeds for mentors and mentees

--- src/database/seeds/004-mentees.js
+++ src/database/seeds/004-mentees.js
@@ -1,15 +1,16 @@
 async function makeMentee(i) {
-  return {
-    user_id: i,
-    wanted_mentor_id: (i % 10) + 1
-  };
+    return {
+        user_id: i,
+        wanted_mentor_id: (i % 10) + 1,
+        status: 'AVAILABLE'
+    };
 }
 
 exports.seed = async (knex, Promise) => {
-  const mentees = [];
-  for (let i = 32; i < 120; i++) {
-    await mentees.push(await makeMentee(i));
-  }
-  console.log("----- MENTEES ADDED -----");
-  return knex("menteeprofiles").insert(mentees);
+    const mentees = [];
+    for (let i = 32; i < 120; i++) {
+        await mentees.push(await makeMentee(i));
+    }
+    console.log('----- MENTEES ADDED -----');
+    return knex('menteeprofiles').insert(mentees);
 };
