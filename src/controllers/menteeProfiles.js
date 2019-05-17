fc56749e7c0bf58f4a9c86012c869b87f871e870
Łukasz Siatka <lukasz@lukaszsiatka.pl>
2019-05-16 19:54:16 -0700

    updated mentee profiles with filtering

--- src/controllers/menteeProfiles.js
+++ src/controllers/menteeProfiles.js
@@ -32,12 +32,18 @@ async function getMenteeProfiles(req, res) {
 
 async function addMenteeProfile(req, res) {
     try {
-        const menteeData = ({ user_id, wanted_mentor_id, status } = req.body);
-        const mentee = await MenteeProfiles.getMenteeProfileByUserId(menteeData.user_id);
+        const current_user = await Users.getUserById(req.user.id);
+        if (current_user === undefined)
+            return await res.status(403).json({ error: 'Access denied!' });
+
+        const menteeProfile = ({ user_id, wanted_mentor_id, status } = req.body);
+        const mentee = await MenteeProfiles.getMenteeProfileByUserId(menteeProfile.user_id);
+
         if (mentee !== undefined)
-            return await res.status(404).json({ error: 'Mentee profile already exist!' });
-        const id = MenteeProfiles.insertMenteeProfile(menteeData);
-        return await res.status(201).json({ id, ...mentee, ...menteeData });
+            return await res.status(404).json({ error: 'Mentee profile already exists!' });
+
+        const id = await MenteeProfiles.insertMenteeProfile(menteeProfile);
+        return await res.status(201).json({ id, ...mentee, ...menteeProfile });
     } catch (error) {
         return await res.status(500).json({ error: error.message });
     }
@@ -45,9 +51,27 @@ async function addMenteeProfile(req, res) {
 
 async function getMenteeProfile(req, res) {
     try {
+        const current_user = await Users.getUserById(req.user.id);
+        if (current_user === undefined)
+            return await res.status(403).json({ error: 'Access denied!' });
+        const all_users = await Users.getAllUsers();
+
         const mentee = await MenteeProfiles.getMenteeProfileById(req.params.id);
-        if (mentee === undefined || mentee.deleted)
-            return await res.status(404).json({ error: 'Mentee not found!' });
+        if (
+            current_user.role === 'ADMINISTRATOR' ||
+            current_user.role === 'OWNER' ||
+            current_user.role === 'MANAGER'
+        ) {
+            if (mentee === undefined)
+                return await res.status(404).json({ error: 'Mentee not found!' });
+        } else {
+            if (mentee === undefined || mentee.deleted)
+                return await res.status(404).json({ error: 'Mentee not found!' });
+
+            const user = all_users.find((elem) => elem.id === mentee.user_id);
+            if (current_user.organization_id !== user.organization_id)
+                return await res.status(403).json({ error: 'Access denied!' });
+        }
         return await res.status(200).json(mentee);
     } catch (error) {
         return await res.status(500).json({ error: error.message });
@@ -56,20 +80,42 @@ async function getMenteeProfile(req, res) {
 
 async function updateMenteeProfile(req, res) {
     try {
-        const menteeData = ({ user_id, wanted_mentor_id, status } = req.body);
+        const current_user = await Users.getUserById(req.user.id);
+        if (current_user === undefined)
+            return await res.status(403).json({ error: 'Access denied!' });
+        const all_users = await Users.getAllUsers();
+
         const mentee = await MenteeProfiles.getMenteeProfileById(req.params.id);
+        if (
+            current_user.role === 'ADMINISTRATOR' ||
+            current_user.role === 'OWNER' ||
+            current_user.role === 'MANAGER'
+        ) {
+            if (mentee === undefined)
+                return await res.status(404).json({ error: 'Mentee not found!' });
+        } else {
+            if (mentee === undefined || mentee.deleted)
+                return await res.status(404).json({ error: 'Mentee not found!' });
+
+            const user = all_users.find((elem) => elem.id === mentee.user_id);
+            if (
+                current_user.organization_id !== user.organization_id ||
+                current_user.id !== mentee.user_id
+            )
+                return await res.status(403).json({ error: 'Access denied!' });
+        }
+
+        const menteeData = ({ user_id, wanted_mentor_id, status } = req.body);
 
         const user = await Users.getUserById(menteeData.user_id);
-        if (user.role !== 'MENTEE' && menteeData.status === 'APPROVED') {
-            await Users.updateUser(menteeData.user_id, {
+        if (user.role !== 'MENTEE' && menteeProfile.status === 'APPROVED') {
+            await Users.updateUser(menteeProfile.user_id, {
                 role: 'MENTEE'
             });
         }
 
-        if (mentee === undefined || mentee.deleted)
-            return await res.status(404).json({ error: 'Mentee not found!' });
-        await MenteeProfiles.updateMenteeProfile(req.params.id, menteeData);
-        return await res.status(200).json({ id: req.params.id, ...menteeData });
+        await MenteeProfiles.updateMenteeProfile(req.params.id, menteeProfile);
+        return await res.status(200).json({ id: req.params.id, ...mentee, ...menteeProfile });
     } catch (error) {
         return await res.status(500).json({ error: error.message });
     }
@@ -77,10 +123,32 @@ async function updateMenteeProfile(req, res) {
 
 async function deleteMenteeProfile(req, res) {
     try {
+        const current_user = await Users.getUserById(req.user.id);
+        if (current_user === undefined)
+            return await res.status(403).json({ error: 'Access denied!' });
+        const all_users = await Users.getAllUsers();
+
         const mentee = await MenteeProfiles.getMenteeProfileById(req.params.id);
-        if (mentee === undefined || mentee.deleted)
-            return await res.status(404).json({ error: 'Mentee not found!' });
-        MenteeProfiles.deleteMenteeProfile(req.params.id);
+        if (
+            current_user.role === 'ADMINISTRATOR' ||
+            current_user.role === 'OWNER' ||
+            current_user.role === 'MANAGER'
+        ) {
+            if (mentee === undefined)
+                return await res.status(404).json({ error: 'Mentee not found!' });
+        } else {
+            if (mentee === undefined || mentee.deleted)
+                return await res.status(404).json({ error: 'Mentee not found!' });
+
+            const user = all_users.find((elem) => elem.id === mentee.user_id);
+            if (
+                current_user.organization_id !== user.organization_id ||
+                current_user.id !== mentee.user_id
+            )
+                return await res.status(403).json({ error: 'Access denied!' });
+        }
+
+        await MenteeProfiles.deleteMenteeProfile(req.params.id);
         return await res.status(200).json({ id: req.params.id });
     } catch (error) {
         return await res.status(500).json({ error: error.message });
@@ -89,10 +157,32 @@ async function deleteMenteeProfile(req, res) {
 
 async function removeMenteeProfile(req, res) {
     try {
+        const current_user = await Users.getUserById(req.user.id);
+        if (current_user === undefined)
+            return await res.status(403).json({ error: 'Access denied!' });
+        const all_users = await Users.getAllUsers();
+
         const mentee = await MenteeProfiles.getMenteeProfileById(req.params.id);
-        if (mentee === undefined || mentee.deleted)
-            return await res.status(404).json({ error: 'Mentee not found!' });
-        MenteeProfiles.removeMenteeProfile(req.params.id);
+        if (
+            current_user.role === 'ADMINISTRATOR' ||
+            current_user.role === 'OWNER' ||
+            current_user.role === 'MANAGER'
+        ) {
+            if (mentee === undefined)
+                return await res.status(404).json({ error: 'Mentee not found!' });
+        } else {
+            if (mentee === undefined || mentee.deleted)
+                return await res.status(404).json({ error: 'Mentee not found!' });
+
+            const user = all_users.find((elem) => elem.id === mentee.user_id);
+            if (
+                current_user.organization_id !== user.organization_id ||
+                current_user.id !== mentee.user_id
+            )
+                return await res.status(403).json({ error: 'Access denied!' });
+        }
+
+        await MenteeProfiles.removeMenteeProfile(req.params.id);
         return await res.status(200).json({ id: req.params.id });
     } catch (error) {
         return await res.status(500).json({ error: error.message });
