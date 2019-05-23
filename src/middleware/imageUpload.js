673e40255b11a083d0d10ee5b5b53f58fce295db
Łukasz Siatka <lukasz@lukaszsiatka.pl>
2019-05-23 01:39:12 -0400

    added image upload gcs middleware

---
+++ src/middleware/imageUpload.js
@@ -0,0 +1,47 @@
+'use strict';
+
+const {Storage} = require('@google-cloud/storage');
+const fs = require('fs');
+require('dotenv').config();
+
+const gcs = new Storage({
+    projectId: 'your-project-id',
+    keyFilename: '/path/to/keyfile.json'
+});
+  
+const bucketName = 'bucket-name-for-upload'
+const bucket = gcs.bucket(bucketName);
+  
+function getPublicUrl(filename) {
+    return 'https://storage.googleapis.com/' + bucketName + '/' + filename;
+}
+  
+let imageUpload = {};
+  
+imageUpload.uploadToGCS = (req, res, next) => {
+    if(!req.file) return next();
+  
+    const gcsname = req.file.originalname;
+    const file = bucket.file(gcsname);
+  
+    const stream = file.createWriteStream({
+        metadata: {
+            contentType: req.file.mimetype
+        }
+    });
+  
+    stream.on('error', (err) => {
+        req.file.cloudStorageError = err;
+        next(err);
+    });
+  
+    stream.on('finish', () => {
+        req.file.cloudStorageObject = gcsname;
+        req.file.cloudStoragePublicUrl = getPublicUrl(gcsname);
+        next();
+    });
+  
+    stream.end(req.file.buffer);
+}
+
+module.exports = imageUpload;
