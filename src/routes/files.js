4aef61bc5a22526914b1fff625dbd03f1f866e0d
Łukasz Siatka <lukasz@lukaszsiatka.pl>
2019-05-23 17:14:20 -0400

    updated files routes

--- src/routes/files.js
+++ src/routes/files.js
@@ -3,22 +3,11 @@ const { filesController } = require('../controllers');
 const authenticate = require('../middleware/authenticate');
 const authorize = require('../middleware/authorize');
 const imageUpload = require('../middleware/imageUpload');
-const multer = require('multer');
-const upload = multer({
-    storage: multer.memoryStorage,
-    fileSize: 5*1024*1024
-});
 
 router.route('/avatar')
-    .post(authenticate, authorize(['ALL']), upload.single('avatar'), imageUpload.uploadToGCS, filesController.uploadAvatar);
-
-router.route('/avatar/:id')
-    .get(authenticate, authorize(['ALL']), filesController.getAvatar);
+    .post(authenticate, authorize(['ALL']), imageUpload.upload.single('avatar'), imageUpload.uploadToGCS, filesController.uploadAvatar);
 
 router.route('/logo')
-    .post(authenticate, authorize(['ALL']), upload.single('logo'), imageUpload.uploadToGCS, filesController.uploadLogo);
-
-router.route('/logo/:id')
-    .post(authenticate, authorize(['ALL']), filesController.getLogo);
+    .post(authenticate, authorize(['ALL']), imageUpload.upload.single('logo'), imageUpload.uploadToGCS, filesController.uploadLogo);
 
 module.exports = router;
