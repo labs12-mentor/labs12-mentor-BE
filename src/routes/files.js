668231a76082c9c5ae8a86336ab8b309d0d134ab
Łukasz Siatka <lukasz@lukaszsiatka.pl>
2019-05-22 23:17:33 -0400

    updated files routes

--- src/routes/files.js
+++ src/routes/files.js
@@ -2,11 +2,16 @@ const router = require('express').Router();
 const { filesController } = require('../controllers');
 const authenticate = require('../middleware/authenticate');
 const authorize = require('../middleware/authorize');
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' });
 
 router.route('/avatar')
-    .post(authenticate, authorize(['ALL']), filesController.uploadAvatar);
+    .post(authenticate, authorize(['ALL']), upload.single('avatar'), filesController.uploadAvatar);
+
+router.route('/avatar/:id')
+    .get(authenticate, authorize(['ALL']), filesController.getAvatar);
 
 router.route('/logo')
-    .post(authenticate, authorize(['ALL']), filesController.uploadLogo);
+    .post(authenticate, authorize(['ALL']), upload.single('logo'), filesController.uploadLogo);
 
 module.exports = router;
