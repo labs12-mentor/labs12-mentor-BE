38869c7fdcd85a6dca97b02ff8eea049e1ca8a20
Łukasz Siatka <lukasz@lukaszsiatka.pl>
2019-05-23 20:48:34 -0400

    updated preinstall

--- preinstall.js
+++ preinstall.js
@@ -5,7 +5,7 @@ const text = `{
     "type":"${process.env.GCS_TYPE}",
     "project_id":"${process.env.GCS_PROJECT_ID}",
     "private_key_id":"${process.env.GCS_PRIVATE_KEY_ID}",
-    "private_key":${JSON.stringify(process.env.GCS_PRIVATE_KEY)},
+    "private_key":${JSON.stringify(process.env.GCS_PRIVATE_KEY).replace(/\\n/g, '\n')},
     "client_email":"${process.env.GCS_CLIENT_EMAIL}",
     "client_id":"${process.env.GCS_CLIENT_ID}",
     "auth_uri":"${process.env.GCS_AUTH_URI}",
